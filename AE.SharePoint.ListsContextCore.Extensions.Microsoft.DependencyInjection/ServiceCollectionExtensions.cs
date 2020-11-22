903e158241c868d87634d6be5f1ce180de3906b6
Александр Епанчинцев <ultra777@mail.ru>
2020-11-22 13:48:48 +0700

    Added xml comments for di project.

--- AE.SharePoint.ListsContextCore.Extensions.Microsoft.DependencyInjection/ServiceCollectionExtensions.cs
+++ AE.SharePoint.ListsContextCore.Extensions.Microsoft.DependencyInjection/ServiceCollectionExtensions.cs
@@ -6,8 +6,17 @@ using AE.SharePoint.ListsContextCore.Infrastructure;
 
 namespace AE.SharePoint.ListsContextCore.Extensions.Microsoft.DependencyInjection
 {
+    /// <summary>
+    /// Extension methods for adding services to an Microsoft.Extensions.DependencyInjection.IServiceCollection.
+    /// </summary>
     public static class ServiceCollectionExtensions
     {
+        /// <summary>
+        /// Adds and configure AE.SharePoint.ListsContextCore.SharePointListsContext.
+        /// </summary>
+        /// <typeparam name="T">The type of context class inherited from AE.SharePoint.ListsContextCore.SharePointListsContext</typeparam>
+        /// <param name="serviceCollection">The Microsoft.Extensions.DependencyInjection.IServiceCollection to add the service to.</param>
+        /// <param name="options">A delegate that is used to configure an AE.SharePoint.ListsContextCore.SharePointListsContext.</param>
         public static void AddSharePointListsContext<T>(this IServiceCollection serviceCollection, Action<Options> options) where T : SharePointListsContext
         {
             if (options == null)
