8261579bc237d33c69b7cf3b0fdc8ade693575da
Александр Епанчинцев <ultra777@mail.ru>
2021-01-26 20:41:24 +0700

    Created base lookup methods.

---
+++ AE.SharePoint.ListsContextCore/SharePointLookupValueAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AE.SharePoint.ListsContextCore
+{
+    
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class SharePointLookupValueAttribute: SharePointFieldTypeAttribute
+    {
+        private string pulledFieldName;
+
+        public SharePointLookupValueAttribute(string pulledFieldName) :base(Infrastructure.SharePointFieldType.LookupValue)
+        {
+            this.pulledFieldName = pulledFieldName;
+        }
+
+        public string PulledFieldName => pulledFieldName;
+    }
+}
